30f0b1c02f74af70793312e0c79c718765217738
wakeup-wakeup <2561814198@qq.com>
2024-10-15 19:16:21 +0800

    solve modules

--- exercises/modules/modules2.rs
+++ exercises/modules/modules2.rs
@@ -9,7 +9,7 @@
 
 
 
-mod delicious_snacks {
+pub mod delicious_snacks {
     // TODO: Fix these use statements
     pub use self::fruits::PEAR as fruit;
     pub use self::veggies::CUCUMBER as veggie;
