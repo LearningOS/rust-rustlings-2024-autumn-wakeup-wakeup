2376e62b3f4da505e8304bec73bc137262789ecf
wakeup-wakeup <2561814198@qq.com>
2024-10-18 11:29:52 +0800

    solve errors

--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -52,7 +52,7 @@ mod my_module {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    use super::transformer;
+    use super::my_module::transformer;
     use super::Command;
 
     #[test]
